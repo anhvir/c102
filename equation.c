5abd9f24b4de048a3a36fa98421812b47b4fec4d
anhvir <anhvir@gmail.com>
2021-08-04 10:13:20 +1000

    Week 2

---
+++ equation.c
@@ -0,0 +1,25 @@
+/* Solving equation ax + b = 0  
+   by Anh Vo, anhvir@gmail.com
+   Last updated: 04 AUG 2017
+
+   Note: this file contains a few errors, you need to fix them first!
+*/	
+
+
+#include <stdio.h>
+
+int main (int argc, char *agrv[]) {
+   float a, b, x;
+   printf ("a, b = "); 
+   scanf("%f %f", a, b);
+   
+   if (a=0 && b=0) {
+      printf ("unlimited number of solutions\n");
+   } else if (a==0) {
+      printf ("no solution\n");
+   } else :
+      printf ("solution x= %f\n", -b/a); 
+       
+   return 0;
+} 
+
